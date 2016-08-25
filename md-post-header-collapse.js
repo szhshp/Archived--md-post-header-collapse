85b9d2a7fa473abf151ef635d8f46a540a7463a1
szhielelp <zillionleo@foxmail.com>
2016-08-25 14:29:36 +0800

    Updated: added a function to make the title collapse at once

--- md-post-header-collapse.js
+++ md-post-header-collapse.js
@@ -1,9 +1,10 @@
-	var arrBreakId =  new Array("uyan_frame");
-	var arrCollapsableTag = new Array("H1","H2","H3");
-	var arrExcludeTagPrefix = new Array("sidebar-toc-Ik4D-");
-	// var excludeTagList = new Array(""); //TODO
+var arrBreakId =  new Array("uyan_frame");
+var arrCollapsableTag = new Array("H1","H2","H3");
+var arrExcludeTagPrefix = new Array("sidebar-toc-Ik4D-");
+// var excludeTagList = new Array(""); //TODO
+function headerCollapsible() {
 	$(':not(blockquote)>h1, :not(blockquote)>h2, :not(blockquote)>h3').each(function(index, curHeader) {
-		if(index!=0)  $(curHeader).prepend('<span class="glyphicon glyphicon-minus headerbtn" aria-hidden="true" title="click to clps and expd"></span>');
+		if(index!=0)  $(curHeader).prepend('<span class="glyphicon glyphicon-minus headerbtn" aria-hidden="true"></span>');
 		/**
 		* [curHeader: current header]
 		* [this: current span icon]
@@ -30,7 +31,7 @@
 				&& (arrCollapsableTag.indexOf(nextElem.prop("tagName")) == -1 || arrCollapsableTag.indexOf(nextElem.prop("tagName")) > tagNameLevel)
 				 ){
 				ignoreFlag = false;
-				console.log(nextElem);
+				// console.log(nextElem);
 				for (var i = 0; i < arrExcludeTagPrefix.length; i++) {
 					if (nextElem.prop('id').indexOf(arrExcludeTagPrefix[i])==0) {
 						ignoreFlag = true;
@@ -39,8 +40,6 @@
 				};
 				if (!ignoreFlag) {
 					displayIt? nextElem.show(400):nextElem.hide(400);
-
-					
 					if (displayIt && !!nextElem.find('span.headerbtn')) {
 						nextElem.find('span.headerbtn').each(function(index, subIcon) {
 							$(subIcon).removeClass('glyphicon-plus');
@@ -56,3 +55,9 @@
 			}
 		});
 	});    
+}
+
+/* Run this function to make the title auto collapse */
+function headerCollapse(level){
+	$(arrCollapsableTag[level]).find('span.headerbtn').click();
+}
